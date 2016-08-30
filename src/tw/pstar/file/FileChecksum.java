05a7f2de26e7ab848b5bcf3e638f0492c80f6f12
PStar <pstar.lin@mail.im.tku.edu.tw>
2016-08-30 16:22:17 +0800

    計算檔案與文字的Hash

---
+++ src/tw/pstar/file/FileChecksum.java
@@ -0,0 +1,83 @@
+package tw.pstar.file;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Set;
+
+public class FileChecksum {
+	private MessageDigest digest;
+
+	public FileChecksum(String algorithm) throws NoSuchAlgorithmException {
+		digest = MessageDigest.getInstance(this.getSupportedAlgorithm().contains(algorithm) ? algorithm : "MD5");
+	}
+
+	public Set<String> getSupportedAlgorithm() {
+		return Security.getAlgorithms("MessageDigest");
+	}
+
+	public String getAlgorithm() {
+		return this.digest.getAlgorithm();
+	}
+
+	public void reset() {
+		this.digest.reset();
+	}
+
+	public String getChecksumString(File file) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		for (byte b : this.getChecksumBytes(file)) {
+			sb.append(String.format("%02X", b));
+		}
+
+		return sb.toString();
+	}
+
+	public String getChecksumString(String text) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		for (byte b : this.getChecksumBytes(text)) {
+			sb.append(String.format("%02X", b));
+		}
+
+		return sb.toString();
+	}
+
+	public byte[] getChecksumBytes(File file) throws IOException {
+		this.calcFileChecksum(file);
+		return this.digest.digest();
+	}
+
+	public byte[] getChecksumBytes(String text) throws IOException {
+		this.calcFileChecksum(text);
+		return this.digest.digest();
+	}
+
+	private void calcFileChecksum(File file) throws IOException {
+		byte[] buff = new byte[1024];
+		FileInputStream in = new FileInputStream(file);
+		DigestInputStream stream = new DigestInputStream(in, this.digest);
+
+		while (stream.read(buff) != -1)
+			;
+		stream.close();
+		in.close();
+	}
+
+	private void calcFileChecksum(String text) throws IOException {
+		byte[] buff = new byte[1024];
+
+		InputStream in = new ByteArrayInputStream(text.getBytes());
+		DigestInputStream stream = new DigestInputStream(in, this.digest);
+
+		while (stream.read(buff) != -1)
+			;
+		stream.close();
+		in.close();
+	}
+}
---
+++ src/tw/pstar/file/FileChecksumExec.java
@@ -0,0 +1,84 @@
+package tw.pstar.file;
+
+import java.io.File;
+import java.security.Security;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+public class FileChecksumExec {
+
+	public static void main(String[] args) throws Exception {
+		Options options = getOptions();
+
+		CommandLine cmd = new DefaultParser().parse(options, args);
+		File file;
+		FileChecksum fileChecksum;
+		String strDigest = new String();
+		String strFile = new String();
+		String text = new String();
+
+		for (Option option : cmd.getOptions()) {
+			switch (option.getOpt()) {
+			case "digest":
+				strDigest = cmd.getOptionValue("digest").toUpperCase();
+
+				if (!Security.getAlgorithms("MessageDigest").contains(strDigest)) {
+					System.err.println("不支援該演算法");
+					System.exit(1);
+				}
+
+				break;
+			case "digestlist":
+				Security.getAlgorithms("MessageDigest").forEach(digest -> System.out.printf("%s ", digest));
+				System.exit(0);
+			case "file":
+				if (new File(cmd.getOptionValue("file")).exists())
+					strFile = cmd.getOptionValue("file");
+				break;
+			case "help":
+				printHelp(options);
+			case "text":
+				text = cmd.getOptionValue("text");
+				break;
+			default:
+				break;
+			}
+		}
+
+		fileChecksum = new FileChecksum(strDigest);
+		if (text.isEmpty() && strFile.isEmpty()) {
+			printHelp(options);
+		} else if (!strFile.isEmpty()) {
+			file = new File(strFile);
+			System.out.printf("%s HASH: %s\n", fileChecksum.getAlgorithm(), fileChecksum.getChecksumString(file));
+		} else if (!text.isEmpty()) {
+			System.out.printf("%s HASH: %s\n", fileChecksum.getAlgorithm(), fileChecksum.getChecksumString(text));
+		}
+	}
+
+	private static Options getOptions() {
+		Options options = new Options();
+
+		options.addOption("digest", true, "雜湊運算演算法，預設：MD5。");
+		options.addOption("digestlist", false, "列出支援的雜湊運算演算法");
+
+		options.addOption("file", true, "檔案路徑");
+		options.getOption("file").setArgName("file path");
+
+		options.addOption("help", false, "顯示HELP訊息");
+
+		options.addOption("text", true, "計算純文字內容");
+		options.getOption("text").setArgName("string");
+
+		return options;
+	}
+
+	private static void printHelp(Options options) {
+		new HelpFormatter().printHelp("java -jar <filename>.jar [OPTION]", options);
+		System.exit(0);
+	}
+}
